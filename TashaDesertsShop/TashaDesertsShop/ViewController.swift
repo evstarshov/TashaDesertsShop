56d996077a7665addfa4bdf274d31e6389290600
EugeneStarshov <yeenzo@yandex.ru>
2022-03-22 11:37:21 +0300

    First commit

---
+++ TashaDesertsShop/TashaDesertsShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TashaDesertsShop
+//
+//  Created by Евгений Старшов on 22.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
