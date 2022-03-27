41479ebd80dad27447259c00a61b9219fa281116
Crocodil <evstarshov@icloud.com>
2022-03-27 12:48:42 +0300

    Auth class, AuthRequestFactory, RequestRouter

--- TashaDesertsShop/TashaDesertsShop/App/AppDelegate.swift
+++ TashaDesertsShop/TashaDesertsShop/App/AppDelegate.swift
@@ -10,11 +10,22 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let requestFactory = RequestFactory()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        
+        let auth = requestFactory.makeAuthRequestFactory()
+        auth.login(userName: "Somebody", password: "mypassword") { response in
+            
+            switch response.result {
+            case .success(let login):
+                print(login)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+            
+        }
+       return true
     }
 
     // MARK: UISceneSession Lifecycle
