8f2ec69a17bcb12b0a8c241b0aa9aa03354f4c55
Crocodil <evstarshov@icloud.com>
2022-03-27 13:35:21 +0300

    ChangeUserData class + Factory

---
+++ TashaDesertsShop/TashaDesertsShop/App/Entity/ChangeUserDataResult.swift
@@ -0,0 +1,12 @@
+//
+//  ChangeUserDataResult.swift
+//  TashaDesertsShop
+//
+//  Created by Евгений Старшов on 27.03.2022.
+//
+
+import Foundation
+
+struct ChangeUserDataResult: Codable {
+    let result: Int
+}
