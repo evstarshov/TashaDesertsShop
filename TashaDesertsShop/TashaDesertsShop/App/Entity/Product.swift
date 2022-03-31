2129a18690a19fa1c7ee264dc60f864aed2d575f
Crocodil <evstarshov@icloud.com>
2022-03-31 12:14:56 +0300

    product entity

---
+++ TashaDesertsShop/TashaDesertsShop/App/Entity/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  TashaDesertsShop
+//
+//  Created by Евгений Старшов on 31.03.2022.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let price: Int?
+    let description: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case price = "product_price"
+        case description = "product_description"
+    }
+}
