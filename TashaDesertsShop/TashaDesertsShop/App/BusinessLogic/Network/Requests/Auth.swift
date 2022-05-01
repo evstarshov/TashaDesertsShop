908bc333dbc2fbf97d7db510147eda8ecfdf888c
Crocodil <evstarshov@icloud.com>
2022-05-01 18:15:58 +0300

    urls fix

--- TashaDesertsShop/TashaDesertsShop/App/BusinessLogic/Network/Requests/Auth.swift
+++ TashaDesertsShop/TashaDesertsShop/App/BusinessLogic/Network/Requests/Auth.swift
@@ -12,7 +12,7 @@ class Auth: AbstractRequestFactory {
     let errorParser: AbstractErrorParser
     let sessionManager: Session
     let queue: DispatchQueue
-    let baseUrl = URL(string: "http://127.0.0.1:8080")!
+    let baseUrl = URL(string: "https://vast-retreat-13451.herokuapp.com/")!
     
     init(
         errorParser: AbstractErrorParser,
