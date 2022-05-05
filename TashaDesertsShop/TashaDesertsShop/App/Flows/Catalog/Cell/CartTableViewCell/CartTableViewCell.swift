07049abf83525feb921caf193ff367a94e9f3fa2
Crocodil <evstarshov@icloud.com>
2022-05-05 12:33:38 +0300

    cart cell class

--- TashaDesertsShop/TashaDesertsShop/App/Flows/Catalog/Cell/CartTableViewCell/CartTableViewCell.swift
+++ TashaDesertsShop/TashaDesertsShop/App/Flows/Catalog/Cell/CartTableViewCell/CartTableViewCell.swift
@@ -8,14 +8,29 @@
 import UIKit
 
 class CartTableViewCell: UITableViewCell {
-
+    
+    // MARK: IBOutlets:
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var productImage: RoundImageView!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
+    public func configure(from model: CartCellModel) {
+        self.productNameLabel.text = model.productName
+        self.priceLabel.text = String(model.price ?? 0)
+        
+        if let imageUrl = URL(string: model.picUrl ?? "https://www.pngjoy.com/pngm/309/5828658_trailer-hd-omg-404-not-found-transparent-png.png") {
+            productImage.loadImage(url: imageUrl)
+        }
+    }
 }
